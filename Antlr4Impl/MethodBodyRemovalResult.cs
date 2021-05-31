e55aa1ded5d6175451f42bbf5b29d62d4500d44d
Max Farsikov <farsikovmax@gmail.com>
2021-05-31 17:06:26 +0300

    code review: added comments, reverted .csproj

--- Antlr4Impl/MethodBodyRemovalResult.cs
+++ Antlr4Impl/MethodBodyRemovalResult.cs
@@ -6,9 +6,8 @@ namespace antlr_parser.Antlr4Impl
 {
     public class MethodBodyRemovalResult
     {
-
         public readonly string Source;
-        
+
         /// <summary>
         /// Index is a position of last symbol of a function declaration header
         /// It is 'Y' for: fun f(x:X):Y  { 
@@ -19,33 +18,45 @@ namespace antlr_parser.Antlr4Impl
         public static MethodBodyRemovalResult From(string source, List<Tuple<int, int>> blocksToRemove)
         {
             List<Tuple<int, int>> topLevelBlocksToRemove = RemoveNested(blocksToRemove);
-            Dictionary<int,string> idxToRemovedMethodBody = ComputeIdxToRemovedMethodBody(topLevelBlocksToRemove, source);
+            Dictionary<int, string> idxToRemovedMethodBody =
+                ComputeIdxToRemovedMethodBody(topLevelBlocksToRemove, source);
             string cleanedText = RemoveBlocks(source, topLevelBlocksToRemove);
             return new MethodBodyRemovalResult(cleanedText, idxToRemovedMethodBody);
-        } 
+        }
+
         static string RemoveBlocks(string source, List<Tuple<int, int>> removeFromTo)
         {
-            return removeFromTo.AsEnumerable() 
+            return removeFromTo.AsEnumerable()
+                // since the list contains char indices, in case if we traverse in original order - each removed block
+                // will affect the next one.
+                // (Assume you have to remove 1-st and 5-th elements. After removing the first one, you have to remove 4-th rather than 5-th, because it was shifted by removal.)
+                // Traversing them in reverse order allows us to avoid this problem.
+                // (Using previous example, after removing 5-th element you have to remove 1-st one. No shifts anymore.) 
                 .Reverse()
+                //for each block it removes a substring (tuple 'fromTo') from a source text
                 .Aggregate(source,
                     (acc, fromTo) => acc.Remove(fromTo.Item1, fromTo.Item2 - fromTo.Item1 + 1));
         }
 
         static Dictionary<int, string> ComputeIdxToRemovedMethodBody(List<Tuple<int, int>> blocksToRemove, string text)
         {
+            //This map is used to restore removed source code
             Dictionary<int, string> idxToRemovedMethodBody = new Dictionary<int, string>();
 
             int removedLength = 0;
             foreach (Tuple<int, int> fromTo in blocksToRemove)
             {
                 int lengthToRemove = fromTo.Item2 - fromTo.Item1 + 1;
+                // 'fromTo.Item1' is an index in original file, where removed source code used to be. Since this index
+                // is shifted (because of previous deletions in the same file) it should be corrected for
+                // 'removedLength`.
                 idxToRemovedMethodBody[fromTo.Item1 - removedLength - 1] = text.Substring(fromTo.Item1, lengthToRemove);
                 removedLength += lengthToRemove;
             }
 
             return idxToRemovedMethodBody;
         }
-        
+
         /// <summary>
         /// Removes nested blocks for removal. For example:
         /// for input: [1..10, 2..5] this method return: [1..10], since
@@ -69,7 +80,7 @@ namespace antlr_parser.Antlr4Impl
 
             return res.ToList();
         }
-        
+
         public MethodBodyRemovalResult(string source, Dictionary<int, string> idxToRemovedMethodBody)
         {
             Source = source;
